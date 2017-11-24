15b70e891120a55260197e097cf1fd68b3f93f8b
Alex Souza <aasouzaconsult@gmail.com>
2017-11-24 16:33:33 -0300

    Update dicas_diversas_IR.py

--- dicas_diversas_IR.py
+++ dicas_diversas_IR.py
@@ -1,5 +1,11 @@
+# Livro
+# http://www.ling.helsinki.fi/kit/2008s/clt231/nltk-0.9.5/doc/en/book.html
+
+#N-Gram
+#http://www.ling.helsinki.fi/kit/2008s/clt231/nltk-0.9.5/doc/en/book.html#n_gram_tagger_index_term
+#http://tetration.xyz/Ngram-Tutorial/
+
 # Janela deslizante
 sent = ['The', 'dog', 'gave', 'John', 'the', 'newspaper']
 n = 3
 [sent[i:i+n] for i in range(len(sent)-n+1)]
-
