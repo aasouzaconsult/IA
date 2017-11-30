02c4ce18d80755efbb816ba214f9e3a0ffd12e2f
Alex Souza <aasouzaconsult@gmail.com>
2017-11-30 10:40:31 -0300

    Update dicas_diversas_IR.py

--- dicas_diversas_IR.py
+++ dicas_diversas_IR.py
@@ -42,6 +42,58 @@ txtAlex_train_tfidf[0].data # ver dados do primeiro "documento"
 txtAlex_vect.vocabulary_
 txtAlex_train_tfidf.toarray()
 
+############
+# Word2Vec #
+############
+from sklearn.decomposition import PCA
+from matplotlib import pyplot
+from gensim.models import Word2Vec
+# define training data
+sentences = [['this', 'is', 'the', 'first', 'sentence', 'for', 'word2vec'],
+			['this', 'is', 'the', 'second', 'sentence'],
+			['yet', 'another', 'sentence'],
+			['one', 'more', 'sentence'],
+			['and', 'the', 'final', 'sentence']]
+
+# train model
+model = Word2Vec(sentences, min_count=1)
+# summarize the loaded model
+print(model)
+# summarize vocabulary
+words = list(model.wv.vocab)
+print(words)
+# access vector for one word
+print(model['sentence'])
+# save model
+model.save('model.bin')
+# load model
+new_model = Word2Vec.load('model.bin')
+print(new_model)
+
+# Vetores do modelo treinado
+X = model[model.wv.vocab]
+
+# Testando similaridade
+model.similarity('sentence','more')
+model.similarity('more','sentence')
+model.similarity('and','for')
+model.similarity('sentence','this')
+model.most_similar('yet')
+
+# fit a 2d PCA model to the vectors
+X = model[model.wv.vocab]
+pca = PCA(n_components=2)
+result = pca.fit_transform(X)
+# create a scatter plot of the projection
+pyplot.scatter(result[:, 0], result[:, 1])
+words = list(model.wv.vocab)
+for i, word in enumerate(words):
+	pyplot.annotate(word, xy=(result[i, 0], result[i, 1]))
+pyplot.show()
+
+# https://machinelearningmastery.com/develop-word-embeddings-python-gensim/
+# https://quomodocumque.wordpress.com/2016/01/15/messing-around-with-word2vec/
+
 #####################
 # Janela deslizante #
 #####################
