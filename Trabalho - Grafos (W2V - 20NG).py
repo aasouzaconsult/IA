014e4c9759546c2eefe430fb6f6864dc18678c0d
Alex Souza <aasouzaconsult@gmail.com>
2017-12-01 11:23:57 -0300

    Update Trabalho - Grafos (W2V - 20NG).py

--- Trabalho - Grafos (W2V - 20NG).py
+++ Trabalho - Grafos (W2V - 20NG).py
@@ -1,13 +1,3 @@
-###############
-## Verificar ##
-###############
-## import modules & set up logging
-#import gensim, logging
-#logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
-#sentences = [['first', 'sentence'], ['second', 'sentence']]
-## train word2vec on the two sentences
-#model = gensim.models.Word2Vec(sentences, min_count=1)
-
 ############
 ## Inicio ##
 ############
